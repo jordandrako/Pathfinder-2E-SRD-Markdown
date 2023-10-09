8c01d3f104e4ab009ec7433073060dd4c7024720
Jordan Janzen <jordan@morels.me>
2023-10-09 10:51:57 -0700

    Initial update script

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd "$SCRIPT_DIR"
+git fetch upstream
+git stash
+git pull upstream main
+git push origin main
+git pop
